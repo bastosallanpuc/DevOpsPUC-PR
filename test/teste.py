cfff25fc4cf7d3ae757e469b5609ff79e129f7bf
bastosallanpuc <allan.bastos@pucpr.edu.br>
2026-04-23 01:08:40 -0300

    Implementando testes unitarios, 2

--- test/teste.py
+++ test/teste.py
@@ -1,13 +1,21 @@
-from src.main import gerar_cnpj_alfanumerico
+from src.main import app, gerar_cnpj_alfanumerico
+from fastapi.testclient import TestClient #precisa disso pra conseguir abrir o /run
+
+client = TestClient(app)
+
+def is_cnpj_valido(cnpj: str) -> bool:
+    #apoio
+    if len(cnpj) != 14:
+        return False
 
 def test_tamanho_do_cnpj_gerado():
-    """Garante que o CNPJ gerado tem exatamente 14 caracteres."""
+    #14 caracteres sem mascara
     cnpj = gerar_cnpj_alfanumerico()
     assert len(cnpj) == 14
 
 
 def test_formato_do_cnpj_gerado():
-    """Verifica se os 12 primeiros caracteres são alfanuméricos e os 2 últimos são números."""
+    #Verifica se os 12 primeiros caracteres são alfanuméricos e os 2 últimos são números.
     cnpj = gerar_cnpj_alfanumerico()
 
     base = cnpj[:12]
@@ -18,6 +26,38 @@ def test_formato_do_cnpj_gerado():
 
 
 def test_retorno_e_uma_string():
-    """Verifica se o retorno da função é do tipo texto (str)."""
+    #retono é string
     cnpj = gerar_cnpj_alfanumerico()
     assert isinstance(cnpj, str)
+
+
+def test_api_run_default():
+    #retorno OK
+    response = client.get("/run")
+    assert response.status_code == 200
+
+    dados = response.json()
+    assert isinstance(dados, list)
+    assert len(dados) == 100
+
+    # Valida pelo menos um dos itens gerados pela API
+    assert is_cnpj_valido(dados[0]) is True
+
+
+def test_api_run_quantidade_personalizada():
+    #quantidade diferente
+    response = client.get("/run?quantidade=5")
+    assert response.status_code == 200
+    assert len(response.json()) == 5
+
+
+def test_api_run_quantidade_invalida_abaixo_minimo():
+    #quantidade invalida
+    response = client.get("/run?quantidade=0")
+    assert response.status_code == 422  #deve retornar 422
+
+
+def test_api_run_quantidade_invalida_acima_maximo():
+    #quantidade invalida
+    response = client.get("/run?quantidade=1001")
+    assert response.status_code == 422
